56095274aeb340a7021e022c2f815b4727b76930
nguyễn văn hải <nvhaik18@gmail.com>
2022-07-13 23:45:44 +0700

    add home page

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,17 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
+/* GET index page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
 /* GET contact page. */
 router.get("/contact", function (req, res, next) {
   res.render("./page/Contact");
+});
+
+router.get("/home", function (req, res, next) {
+  res.render("./page/Home", { title: "Express" });
 });
 
 module.exports = router;
