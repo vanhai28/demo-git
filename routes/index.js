27cbeafc711b04f9d4272694e3d66652306df087
nguyễn văn hải <nvhaik18@gmail.com>
2022-07-13 23:45:44 +0700

    test 01

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+/* GET index page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/home", function (req, res, next) {
+  res.render("./page/Home", { title: "Express" });
 });
 
 module.exports = router;
