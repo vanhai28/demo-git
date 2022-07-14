6e5975e3383a6cb89757676c83a4ae54e38bdef4
nguyễn văn hải <nvhaik18@gmail.com>
2022-07-14 20:38:11 +0700

    add comment

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,19 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* GET index page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 /* GET contact page. */
 router.get("/contact", function (req, res, next) {
   res.render("./page/Contact");
 });
 
+/* GET home page. */
 router.get("/home", function (req, res, next) {
   res.render("./page/Home", { title: "Express" });
 });
 
 module.exports = router;
+// comment 01
