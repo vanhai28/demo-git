dcc489cd05f8aff33e88ae77e3350d06831a9763
nguyễn văn hải <nvhaik18@gmail.com>
2022-07-14 20:38:11 +0700

    Modify Merge branch 'main' into feature/home-page

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,16 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* GET index page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 /* GET contact page. */
 router.get("/contact", function (req, res, next) {
   res.render("./page/Contact");
 });
 
+/* GET home page. */
 router.get("/home", function (req, res, next) {
   res.render("./page/Home", { title: "Express" });
 });
