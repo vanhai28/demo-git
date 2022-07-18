606dd454b93e1367b37afe3f5fa3e56ca5f558cc
nguyễn văn hải <nvhaik18@gmail.com>
2022-07-18 21:28:00 +0700

    modify index 2
    modify index 3

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,18 @@ var express = require("express");
 var router = express.Router();
 
 /* GET index page. */
+
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
 /* GET contact page. */
+
 router.get("/contact", function (req, res, next) {
   res.render("./page/Contact");
 });
 
 /* GET home page. */
+
 router.get("/home", function (req, res, next) {
   res.render("./page/Home", { title: "Express" });
 });
